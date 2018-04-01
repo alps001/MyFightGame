e7b8fb74a423a60fb8d826e638b0d19b9d4c506c
DESKTOP-T9F323V\yue <584253937@qq.com>
2018-04-02 01:10:18 +0800

    准备添加跑动作

---
+++ Assets/Main/Editor/MoveEditorWindow.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+namespace MyFightGame { 
+    public class MoveEditorWindow : EditorWindow
+    {
+        public static MoveEditorWindow moveEditorWindow;
+        [MenuItem("Window/MyFightGameConfig/MoveInfo Editor")]
+        public static void Init()
+        {
+            moveEditorWindow = EditorWindow.GetWindow<MoveEditorWindow>(false, "MoveInfo", true);
+            moveEditorWindow.Show();
+        
+        }
+
+        public void OnGUI()
+        {
+            if (GUILayout.Button("Create new MoveInfo Configuration"))
+                ScriptableObjectUtility.CreateAsset<MoveInfo>();
+        }
+    }
+}
